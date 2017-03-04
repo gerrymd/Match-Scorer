27575917078276ab065b4ed27a6f0325d23581a6
gerrymd <gerrydigges@gmail.com>
2017-03-04 15:45:01 +0530

    feat: Add Score Variables & Menu Reset

--- app/src/main/java/com/gerry/matchscorer/MainActivity.java
+++ app/src/main/java/com/gerry/matchscorer/MainActivity.java
@@ -2,12 +2,41 @@ package com.gerry.matchscorer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int mScoreA;
+    private int mScoreB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        new MenuInflater(this).inflate(R.menu.scorer_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_reset:
+                resetScores();
+                return true;
+        }
+        return false;
+    }
+
+    private void resetScores(){
+        mScoreA = 0;
+        mScoreB = 0;
     }
 }
